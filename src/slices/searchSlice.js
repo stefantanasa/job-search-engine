d05ab5f1c9fb1db01fb5e21f4c720dfe0826d1b0
stefantanasa <liketanasa@gmail.com>
2022-05-13 12:24:16 +0100

    update

--- src/slices/searchSlice.js
+++ src/slices/searchSlice.js
@@ -1,4 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+
+
+const getResult= createAsyncThunk(
+  "search/getResult",
+  async(url,thunkAPI)=>{
+    try {
+      let response =async fetch(url)
+      if (response.ok){
+        let data = await response.json()
+      }
+      
+    } catch (error) {
+      console.log("❌ There is an error: ", error)
+    }
+  }
+)
 
 const searchSlice = createSlice({
   name: "search",
@@ -10,4 +27,4 @@ const searchSlice = createSlice({
   reducers: {},
 });
 
-export default favSlice.reducer;
+export default searchSlice.reducer;
